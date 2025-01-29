cbb4d63c31c37a45f9adc0299bf9d97fdb25fa5d
Arpit Verma <arpitv970@gmail.com>
2025-01-29 12:07:09 +0530

    updated work

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from "@/lib/mongoose";
+import connectToDatabase from "@/lib/mongodb";
 import User from "@/models/User";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
@@ -7,9 +7,12 @@ export async function POST(req) {
   const { email, password } = await req.json();
 
   if (!email || !password) {
-    return new Response(JSON.stringify({ message: "Missing email or password" }), {
-      status: 400,
-    });
+    return new Response(
+      JSON.stringify({ message: "Missing email or password" }),
+      {
+        status: 400,
+      },
+    );
   }
 
   try {
@@ -17,18 +20,22 @@ export async function POST(req) {
 
     const user = await User.findOne({ email });
     if (!user) {
-      return new Response(JSON.stringify({ message: "User not found" }), { status: 404 });
+      return new Response(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      });
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return new Response(JSON.stringify({ message: "Invalid credentials" }), { status: 401 });
+      return new Response(JSON.stringify({ message: "Invalid credentials" }), {
+        status: 401,
+      });
     }
 
     const token = jwt.sign(
       { id: user._id, email: user.email },
       process.env.JWT_SECRET,
-      { expiresIn: "1h" }
+      { expiresIn: "1h" },
     );
 
     return new Response(JSON.stringify({ token }), { status: 200 });
